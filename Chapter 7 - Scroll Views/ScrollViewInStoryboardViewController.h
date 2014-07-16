dd78ff20a64e43086852aa0bfed2a37b7f9d579d
Dan Wallace <daniel.wallace@datacom.co.nz>
2014-07-16 15:23:42 +1200

    add scroll view in nib

---
+++ Chapter 7 - Scroll Views/ScrollViewInStoryboardViewController.h
@@ -0,0 +1,13 @@
+//
+//  ScrollViewInStoryboardViewController.h
+//  Chapter 7 - Scroll Views
+//
+//  Created by Daniel Wallace [DATACOM] on 16/07/14.
+//  Copyright (c) 2014 Daniel Wallace [DATACOM]. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ScrollViewInStoryboardViewController : UIViewController
+
+@end
