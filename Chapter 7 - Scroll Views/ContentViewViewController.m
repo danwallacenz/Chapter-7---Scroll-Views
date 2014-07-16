f185eca96b8e4585ac0563e9dfe0797324c71f93
Dan Wallace <daniel.wallace@datacom.co.nz>
2014-07-16 13:21:38 +1200

    Set content view using explicit constraints, but its subviews don’t.

--- Chapter 7 - Scroll Views/ContentViewViewController.m
+++ Chapter 7 - Scroll Views/ContentViewViewController.m
@@ -40,10 +40,18 @@ const float LABEL_COUNT = 50;
     [self createScrollViewInView:self.scrollViewContainer2 withColor: [UIColor blueColor]];
     [self createScrollViewInView:self.scrollViewContainer3 withColor: [UIColor orangeColor]];
  
+    // Set content view frame and scroll view contentSize explicitly.
     [self loadScrollViewWithNoConstraintsInView: self.scrollViewContainer0];
+    
+    // Set content view using explicit constraints, but its subviews don’t.
+    [self loadScrollViewWithContentViewWithConstraintsButSubviewsDoNotInView: self.scrollViewContainer1];
     
 }
 
+
+/**
+ Set content view frame and scroll view contentSize explicitly.
+*/
 -(void) loadScrollViewWithNoConstraintsInView: (UIView *)containerView
 {
     CGFloat y = 10;
@@ -51,9 +59,10 @@ const float LABEL_COUNT = 50;
     UIScrollView *scrollView = containerView.subviews[0];
     UIView *contentView = scrollView.subviews[0];
     
+    // Create labels, keep incrementing y.
     for (int i = 0; i < LABEL_COUNT; i++) {
         UILabel *label = [UILabel new];
-        label.text = [NSString stringWithFormat: @"No Constraints %d", i+1 ];
+        label.text = [NSString stringWithFormat: @"No constraints %d", i+1 ];
         
         [label sizeToFit];
         
@@ -61,7 +70,7 @@ const float LABEL_COUNT = 50;
         labelFrame.origin = CGPointMake(10, y);
         label.frame = labelFrame;
         
-//        label.backgroundColor = contentView.backgroundColor;
+        label.backgroundColor = contentView.backgroundColor;
         
         [contentView addSubview:label];
         y += label.bounds.size.height + 10;
@@ -72,6 +81,47 @@ const float LABEL_COUNT = 50;
         contentView.frame = CGRectMake(0, 0, 0, y);
         scrollView.contentSize = contentView.frame.size;
     }
+}
+
+/**
+ Set content view using explicit constraints, but its subviews don’t.
+ */
+-(void) loadScrollViewWithContentViewWithConstraintsButSubviewsDoNotInView: (UIView *)containerView
+{
+    CGFloat y = 10;
+    
+    UIScrollView *scrollView = containerView.subviews[0];
+    UIView *contentView = scrollView.subviews[0];
+    
+    for (int i = 0; i < LABEL_COUNT; i++) {
+        UILabel *label = [UILabel new];
+        label.text = [NSString stringWithFormat: @"ContentView uses constraints, labels don't %d", i+1 ];
+        
+        [label sizeToFit];
+        
+        CGRect labelFrame = label.frame;
+        labelFrame.origin = CGPointMake(10, y);
+        label.frame = labelFrame;
+        
+        label.backgroundColor = contentView.backgroundColor;
+        
+        [contentView addSubview:label];
+        y += label.bounds.size.height + 10;
+    }
+    // Configure the content view using constraints.
+    contentView.translatesAutoresizingMaskIntoConstraints = NO;
+    
+    // Pin contentView to the top and bottom of the scroll view with a height of y.
+    [scrollView addConstraints: [NSLayoutConstraint constraintsWithVisualFormat: @"V:|[contentView(y)]|"
+                                                                        options: 0
+                                                                        metrics: @{@"y":@(y)}
+                                                                          views: @{@"contentView": contentView}]];
+    
+    // Pin contentView to the left and right sides of the scroll view with a width of 0.
+    [scrollView addConstraints: [NSLayoutConstraint constraintsWithVisualFormat: @"H:|[contentView(0)]|"
+                                                                        options: 0
+                                                                        metrics: nil
+                                                                          views: @{@"contentView": contentView}]];
 }
 
 
@@ -96,7 +146,7 @@ const float LABEL_COUNT = 50;
     UIView *contentView = [UIView new];
     contentView.backgroundColor = color;
     [scrollView addSubview:contentView];
-    contentView.frame =CGRectMake(0, 0, 100, 100);
+    contentView.frame =CGRectMake(0, 0, 100, 100); // sanity check - remove this
 }
 
 
