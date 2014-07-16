7a96026fa74eba3e5915441bac357665995a22ca
Dan Wallace <daniel.wallace@datacom.co.nz>
2014-07-16 13:58:07 +1200

    Using explicit constraints throughout.

--- Chapter 7 - Scroll Views/ContentViewViewController.m
+++ Chapter 7 - Scroll Views/ContentViewViewController.m
@@ -37,7 +37,7 @@ const float LABEL_COUNT = 50;
     // Create four scroll views with content views with different colors.
     [self createScrollViewInView:self.scrollViewContainer0 withColor: [UIColor greenColor]];
     [self createScrollViewInView:self.scrollViewContainer1 withColor: [UIColor redColor]];
-    [self createScrollViewInView:self.scrollViewContainer2 withColor: [UIColor blueColor]];
+    [self createScrollViewInView:self.scrollViewContainer2 withColor: [UIColor lightGrayColor]];
     [self createScrollViewInView:self.scrollViewContainer3 withColor: [UIColor orangeColor]];
  
     // Set content view frame and scroll view contentSize explicitly.
@@ -46,8 +46,74 @@ const float LABEL_COUNT = 50;
     // Set content view using explicit constraints, but its subviews don’t.
     [self loadScrollViewWithContentViewWithConstraintsButSubviewsDoNotInView: self.scrollViewContainer1];
     
+    // Using explicit constraints throughout.
+    [self loadScrollViewWithContentViewUsingExplicitConstraintsThroughoutInView:self.scrollViewContainer2];
+    
 }
 
+/**
+ Using explicit constraints throughout.
+ */
+-(void) loadScrollViewWithContentViewUsingExplicitConstraintsThroughoutInView: (UIView *)containerView
+{
+    
+    UIScrollView *scrollView = containerView.subviews[0];
+    UIView *contentView = scrollView.subviews[0];
+    
+    UILabel *previousLabel = nil;
+    
+    for (int i = 0; i < LABEL_COUNT; i++) {
+        
+        UILabel *label = [UILabel new];
+        label.translatesAutoresizingMaskIntoConstraints = NO;
+        
+        label.text = [NSString stringWithFormat: @"Using explicit constraints throughout. %d", i+1 ];
+        label.backgroundColor = contentView.backgroundColor;
+        [contentView addSubview:label];
+        
+        // Pin label to left side with space of 10.
+        [contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat: @"H:|-(10)-[label]"
+                                                                            options: 0
+                                                                            metrics: nil
+                                                                              views: @{@"label":label}]];
+        if(!previousLabel){ // First label, pin to top.
+            [contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat: @"V:|-(10)-[label]"
+                                                                                options: 0
+                                                                                metrics: nil
+                                                                                  views: @{@"label":label}]];
+        }else{
+            // All other labels, pin to previous label.
+            [contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat: @"V:[previousLabel]-(10)-[label]"
+                                                                                options: 0
+                                                                                metrics: nil
+                                                                                  views: @{@"label":label, @"previousLabel":previousLabel}]];
+        }
+        previousLabel = label;
+    }
+    
+    // Last one, pin to bottom. This dictates contentView height.
+    [contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat: @"V:[previousLabel]-(10)-|"
+                                                                        options: 0
+                                                                        metrics: nil
+                                                                          views: @{@"previousLabel":previousLabel}]];
+    
+    
+    
+    // Configure the content view using constraints.
+    contentView.translatesAutoresizingMaskIntoConstraints = NO;
+    
+    // Pin contentView to the top and bottom of the scroll view with a height of y.
+    [scrollView addConstraints: [NSLayoutConstraint constraintsWithVisualFormat: @"V:|[contentView]|"
+                                                                        options: 0
+                                                                        metrics: nil
+                                                                          views: @{@"contentView": contentView}]];
+    
+    // Pin contentView to the left and right sides of the scroll view with a width of 0.
+    [scrollView addConstraints: [NSLayoutConstraint constraintsWithVisualFormat: @"H:|[contentView(0)]|"
+                                                                        options: 0
+                                                                        metrics: nil
+                                                                          views: @{@"contentView": contentView}]];
+}
 
 /**
  Set content view frame and scroll view contentSize explicitly.
@@ -82,6 +148,7 @@ const float LABEL_COUNT = 50;
         scrollView.contentSize = contentView.frame.size;
     }
 }
+
 
 /**
  Set content view using explicit constraints, but its subviews don’t.
