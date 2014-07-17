dcee50f925ce8cb32a4cc785b1bb5d4902a3de03
Daniel Wallace <daniel.wallace.nz@gmail.com>
2014-07-18 07:06:45 +1200

    add mona lisa with explicit constraints

---
+++ Chapter 7 - Scroll Views/MonaLisaViewController.h
@@ -0,0 +1,13 @@
+//
+//  MonaLisaViewController.h
+//  Chapter 7 - Scroll Views
+//
+//  Created by Daniel Wallace on 17/07/14.
+//  Copyright (c) 2014 Daniel Wallace [DATACOM]. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MonaLisaViewController : UIViewController
+
+@end
